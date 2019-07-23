5d2655e6ed2fefc2116f9c25069da314a309f7f2
Pham Lap <lappv@topica.edu.vn>
2019-07-23 11:23:53 +0700

    add

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar
+} from "react-native";
+
+import { Provider } from "react-redux";
+import store from "./src/store";
+import Main from "./src/Main";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Main />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
